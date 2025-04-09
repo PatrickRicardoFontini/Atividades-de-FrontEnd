760e1cbc51b7de456908fe6b1ef598765951c8b0
PatrickRicardoFontini <161254473+PatrickRicardoFontini@users.noreply.github.com>
2025-04-09 12:51:07 -0300

    Atividade 2

---
+++ components/ProfileCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './ProfileCard.module.css';
+
+interface ProfileCardProps {
+  image: string;
+  name: string;
+  description: string;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({ image, name, description }) => {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={name} className={styles.image} />
+      <h2>{name}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
